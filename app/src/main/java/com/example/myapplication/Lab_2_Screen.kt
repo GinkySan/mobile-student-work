8b87b32d478a8bbd2cc259d9f8c4acd2af44ac85
Ginky <krosh024@gmail.com>
2024-10-22 21:55:43 +0300

    lab_2

---
+++ app/src/main/java/com/example/myapplication/Lab_2_Screen.kt
@@ -0,0 +1,57 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Done
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+@Composable
+fun Lab_2_Screen(navController: NavHostController) {
+    val buttonClickState = remember{ mutableStateOf(false) }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Button(
+            onClick = {
+                buttonClickState.value = !buttonClickState.value
+            },
+            modifier = Modifier
+                .padding(bottom = 50.dp)
+        ) {
+            Text(
+                text = "Кнопка"
+            )
+        }
+        Text(
+            text = "Нажмите кнопку"
+        )
+        if (buttonClickState.value) {
+
+            Icon(
+                imageVector = Icons.Rounded.Done,
+                contentDescription = "Done",
+                modifier = Modifier
+                    .padding(top = 50.dp)
+            )
+        }
+    }
+}
